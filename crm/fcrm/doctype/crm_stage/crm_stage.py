20f67b323e7b205ffcd70784f323759c2fa00c17
shahid8025 <madhiya.ayaz@gmail.com>
2025-02-13 12:11:24 +0530

    Create stage relatef fields on create stage,stage fields updates code in above commit

--- crm/fcrm/doctype/crm_stage/crm_stage.py
+++ crm/fcrm/doctype/crm_stage/crm_stage.py
@@ -17,7 +17,54 @@ class CRMStage(Document):
 		exist = frappe.db.exists('CRM Stage', {'stage_name':self.stage_name,"pipeline":self.pipeline})
 		if exist and self.stage_name != stage_name:
 			frappe.throw("Stage Name Already Exist with Selected Pipeline!")
+	
+	def after_insert(self):
+		doc = self
+		fields_to_create = [
+			{
+				"fieldname": f"date_entered_in_{doc.name}",
+				"label": f'Date Entered in "{doc.stage_name} ({doc.pipeline})"',
+				"fieldtype": "Datetime",
+				"insert_after": "stage",
+				"read_only": 1
+			},
+			{
+				"fieldname": f"date_exited_in_{doc.name}",
+				"label": f'Date Exited in "{doc.stage_name} ({doc.pipeline})"',
+				"fieldtype": "Datetime",
+				"insert_after": f"date_entered_in_{doc.name}",
+				"read_only": 1
+			},
+			{
+				"fieldname": f"cumulative_time_in_{doc.name}",
+				"label": f'Cumulative Time in "{doc.stage_name} ({doc.pipeline})"',
+				"fieldtype": "Int",
+				"insert_after": f"date_exited_in_{doc.name}",
+				"read_only": 1
+			},
+			{
+				"fieldname": f"latest_time_in_{doc.name}",
+				"label": f'Latest Time in "{doc.stage_name} ({doc.pipeline})"',
+				"fieldtype": "Int",
+				"insert_after": f"cumulative_time_in_{doc.name}",
+				"read_only": 1
+			}
+		]
+		for field in fields_to_create:
+			if not frappe.db.exists("Custom Field", {"dt": "CRM Deal", "fieldname": field["fieldname"]}):
+				custom_field = frappe.get_doc({
+					"doctype": "Custom Field",
+					"dt": "CRM Deal",
+					"fieldname": field["fieldname"],
+					"label": field["label"],
+					"fieldtype": field["fieldtype"],
+					"insert_after": field["insert_after"],
+					"read_only": field.get("read_only", 0)
+				})
+				custom_field.insert()
+	
 
+		
 
 @frappe.whitelist()
 def sortExist(pipeline,sort):
